b16a6db773f1cc9e6a06603ea0bcf968997b2fa1
avbds002 <avbds2077@gmail.com>
2025-07-02 13:15:36 -0300

    finished adding categories and categories resources

--- src/database/migrations/20250702144604-create-categories-table.js
+++ src/database/migrations/20250702144604-create-categories-table.js
@@ -1,21 +1,36 @@
-'use strict';
+// src/database/migrations/XXXXXXXXXXXXXX-create-categories-table
+
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("categories", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      position: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("categories");
+  },
 };
